cea5e737a639fc6f2689de1b3ca4135b32adc707
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-02-22 08:10:25 +0100

    Delete bulk files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,20 +3,15 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/api/joke.ts";
-import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/log/[id].tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/index.tsx";
+import * as $1 from "./routes/log/[id].tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/joke.ts": $0,
-    "./routes/index.tsx": $1,
-    "./routes/log/[id].tsx": $2,
+    "./routes/index.tsx": $0,
+    "./routes/log/[id].tsx": $1,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
